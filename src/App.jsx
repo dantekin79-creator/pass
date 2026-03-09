a5b0a71c2cff4c4f6caad2ef5e2cb45fecd5b365
dantekin79-creator <dantekin79@gmail.com>
2026-03-09 19:40:03 +0300

    Refactor: Move components to App.jsx for autotest compatibility and exact console logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import PasswordInput from './components/PasswordInput'
-import SubmitButton from './components/SubmitButton'
-import './App.css'
+import React from 'react';
+import './App.css';
+
+const PasswordInput = () => {
+  const handleChange = () => {
+    console.log("Entering password…");
+  };
+
+  return (
+    <input 
+      type="password" 
+      onChange={handleChange} 
+    />
+  );
+};
+
+const SubmitButton = () => {
+  const handleEnter = () => {
+    console.log("Mouse Entering");
+  };
+
+  const handleLeave = () => {
+    console.log("Mouse Exiting");
+  };
+
+  return (
+    <button 
+      onMouseEnter={handleEnter} 
+      onMouseLeave={handleLeave}
+    >
+      Submit Password
+    </button>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Password Anti-Bot Security</h1>
+    <>
       <PasswordInput />
       <SubmitButton />
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
